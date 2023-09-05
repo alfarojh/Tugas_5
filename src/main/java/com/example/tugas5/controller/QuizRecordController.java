0fbcc17e9c870adab68d852d8203ed8bda438ee4
alfarojh <alfarous.jh@gmail.com>
2023-09-05 08:57:36 +0700

    fix API quiz-records

--- src/main/java/com/example/tugas5/controller/QuizRecordController.java
+++ src/main/java/com/example/tugas5/controller/QuizRecordController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,22 @@ public class QuizRecordController {
                         "All list Course.",
                         quizRecordService.quizRecordList()
                 ));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getScoresById(@PathVariable long id) {
+        if (quizRecordService.getQuizRecordById(id) != null) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ApiResponse(
+                            quizRecordService.getMessage(),
+                            quizRecordService.getQuizRecordById(id)
+                    ));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponse(
+                            quizRecordService.getMessage()
+                    ));
+        }
     }
 
     @PostMapping("")
